8798dcd4489d55ce725aec37057bf747df99de3d
Jared588 <jaredeprice@gmail.com>
2023-09-11 11:25:42 +0200

    Implement full "add-to-library" functionality

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const myLibrary = [];
 
     function Book(author, title, pages, status) {
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.status = status
-    return;
+        this.author = author
+        this.title = title
+        this.pages = pages
+        this.status = status
+        return;
     }
 
     function addBookToLibrary(author, title, pages, status) {
@@ -15,13 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Get references to elements
-    var openButton = document.getElementById("add-button");
+    var addButton = document.getElementById("add-button");
     var modal = document.getElementById("myModal");
     var closeButton = document.getElementById("closeButton");
     var form = document.getElementById("myForm");
 
     // Open the modal when the button is clicked
-    openButton.onclick = function() {
+    addButton.onclick = function() {
         modal.style.display = "flex";
     }
 
@@ -40,6 +40,81 @@ document.addEventListener("DOMContentLoaded", function() {
     // Prevent form submission (you can handle form submission as needed)
     form.onsubmit = function(event) {
         event.preventDefault();
-        // Handle form submission logic here
+        var author = document.getElementById("author").value;
+        var title = document.getElementById("title").value;
+        var pages = document.getElementById("pages").value;
+        var status = document.getElementById("status").value;
+
+        addBookToLibrary(author, title, pages, status);
+        modal.style.display = "none";
+        console.log(myLibrary);
+        console.log("test");
+        updateLibrary();
     }
+
+    // Display all books in library
+    function updateLibrary() {
+        let booknum = 0;
+        const bookContainer = document.querySelector(".book-container");
+        bookContainer.innerHTML = "";
+
+        for (const book of myLibrary) {
+            // Create a new div element with the class "book"
+            const bookDiv = document.createElement("div");
+            bookDiv.className = "book";
+
+            // Create the image container div
+            const imageContainerDiv = document.createElement("div");
+            imageContainerDiv.className = "image-container";
+
+            // Create the "Plan To Read" button
+            const readToggleButton = document.createElement("button");
+            readToggleButton.id = "read-toggle";
+            readToggleButton.textContent = book.status;
+
+            // Create the book icon image
+            const bookIconImg = document.createElement("img");
+            bookIconImg.src = "icons/book-open-blank-variant.svg";
+            bookIconImg.alt = "open book icon";
+            bookIconImg.id = "book-icon";
+
+            // Create the "Delete" button
+            const deleteButton = document.createElement("button");
+            deleteButton.id = "delete";
+            const deleteButtonImg = document.createElement("img");
+            deleteButtonImg.src = "icons/delete-outline.svg";
+            deleteButtonImg.alt = "trash can";
+            deleteButton.appendChild(deleteButtonImg);
+
+            // Create the information container div
+            const informationContainerDiv = document.createElement("div");
+            informationContainerDiv.className = "information-container";
+
+            // Create and add the <p> elements with book information
+            const authorParagraph = document.createElement("p");
+            authorParagraph.id = "author"
+            authorParagraph.innerHTML = book.author;
+
+            const titleParagraph = document.createElement("p");
+            titleParagraph.textContent = book.title;
+
+            const pagesParagraph = document.createElement("p");
+            pagesParagraph.textContent = book.pages;
+
+            // Append all elements to their respective containers
+            imageContainerDiv.appendChild(readToggleButton);
+            imageContainerDiv.appendChild(bookIconImg);
+            imageContainerDiv.appendChild(deleteButton);
+
+            informationContainerDiv.appendChild(authorParagraph);
+            informationContainerDiv.appendChild(titleParagraph);
+            informationContainerDiv.appendChild(pagesParagraph);
+
+            bookDiv.appendChild(imageContainerDiv);
+            bookDiv.appendChild(informationContainerDiv);
+
+            // Finally, append the entire "book" div to the document body (or another desired location)
+            bookContainer.appendChild(bookDiv);
+        }
+    }  
 });
